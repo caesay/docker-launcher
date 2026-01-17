8aa66ccea81bbff575abb167ecd78614bd6b4aa5
Caelan Sayler <git@caesay.com>
2026-01-17 23:22:34 +0000

    Make the error page a little more familiar

--- web/src/components/GetStarted.vue
+++ web/src/components/GetStarted.vue
@@ -2,16 +2,7 @@
   <article>
     <div class="m-6 has-text-centered py-6 title">
       <p class="is-size-5 mb-2">No configuration found!</p>
-      <p>Check out the documentation to start building your dashboard.</p>
-      <p>
-        <a
-          class="button is-primary mt-5 has-text-weight-bold"
-          href="https://github.com/bastienwirtz/homer/blob/main/docs/configuration.md#configuration"
-          target="_blank"
-        >
-          Get started →
-        </a>
-      </p>
+      <p>Please try reloading the page, or contact me if the issue persists.</p>
     </div>
   </article>
 </template>
